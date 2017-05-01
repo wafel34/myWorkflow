7ceb2d133926a809c6e63719da00379085b903dc
Pawel Maricnkowski <wafel34@gmail.com>
2017-05-01 11:40:58 +0100

    Browserify setup + js and sass processing

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,62 @@
-var gulp        = require('gulp'),
-    browserify  = require('gulp-browserify'),
-    minifyCss   = require('gulp-clean-css'),
-    concat      = require('gulp-concat'),
-    gulpIf      = require('gulp-if'),
-    plumber     = require('gulp-plumber'),
-    sass        = require('gulp-sass'),
-    uglify      = require('gulp-uglify'),
-    watch       = require('gulp-watch'),
-    htmlmin     = require('gulp-htmlmin'),
-    browserSync = require('browser-sync').create();
+/*---------------------------
+        MODULES
+---------------------------*/
+
+var gulp         = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserify   = require('gulp-browserify'),
+    minifyCss    = require('gulp-clean-css'),
+    concat       = require('gulp-concat'),
+    gulpIf       = require('gulp-if'),
+    plumber      = require('gulp-plumber'),
+    sass         = require('gulp-sass'),
+    uglify       = require('gulp-uglify'),
+    watch        = require('gulp-watch'),
+    htmlmin      = require('gulp-htmlmin'),
+    browserSync  = require('browser-sync').create();
+
+/*---------------------------
+        TASKS
+---------------------------*/
+
+var sassSources,
+    jsSources,
+    cssDest,
+    outputDir;
+
+
+jsSources    = 'components/scripts/*.js';
+sassSources  = 'components/sass/*.sass';
+
+
+//SASS TO CSS
+gulp.task('sass', function () {
+    return gulp.src(sassSources)
+            .pipe(plumber())
+            .pipe(sass())
+            .pipe(gulp.dest('builds/development/css'))
+});
+
+//CONCAT JS
+
+gulp.task('js', function () {
+   return gulp.src(jsSources)
+            .pipe(plumber())
+            .pipe(concat('main.js'))
+            .pipe(browserify())
+            .pipe(gulp.dest('builds/development/js'))
+});
+
+
+
+/*---------------------------
+        WATCH TASK
+---------------------------*/
+
+
+
+
+
+/*---------------------------
+        DEFAULT TASK
+---------------------------*/
